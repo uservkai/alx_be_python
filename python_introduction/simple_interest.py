55b1bd7fdc51987126e59f8ee61749edd25f2ae0
uservkai <katemary6623@gmail.com>
2025-07-13 15:23:09 +0300

    updated names

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,6 +1,6 @@
-P = 1000 #principal is $1000
-R = 0.05 #rate is 5% pa
-T = 3 #time is 3 years
+principal = 1000 #principal is $1000
+rate = 0.05 #rate is 5% pa
+time = 3 #time is 3 years
 
-I = P * R * T #simple interest interest
-print ('The simple interest is', I)
+interest = principal * rate * time #simple interest interest
+print ('The simple interest is', interest)
