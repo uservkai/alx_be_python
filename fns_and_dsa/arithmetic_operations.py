5e9775fd86631baae183970e38f9fb609de627a2
uservkai <katemary6623@gmail.com>
2025-07-26 22:29:32 +0300

    added invalid operation case

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -6,9 +6,11 @@ def perform_operation(num1, num2, operation):
         result = num1 - num2
     elif operation == "multiply":
         result = num1 * num2
-    else:
+    elif operation == "divide":
         if num2 == 0:
             result = "Cannot divide by 0"
         else:
             result = num1 / num2
+    else:
+        result = "Invalid operation"
     return result
