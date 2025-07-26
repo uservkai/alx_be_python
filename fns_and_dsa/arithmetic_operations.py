8ffb7720f712ba4615421c2d3a08d5415b12e8a7
uservkai <katemary6623@gmail.com>
2025-07-26 22:25:08 +0300

    Changed conditional from match-case to if

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,15 +1,14 @@
 def perform_operation(num1, num2, operation):
-    match operation:
-        case "add":
-            operation = num1 + num2
-        case "subtract":
-            operation = num1 - num2
-        case "multiply":
-            operation = num1 * num2
-        case "divide":
-            if num2 == 0:
-                message = "Cannot divide by 0"
-                return message
-            else:
-                operation = num1 / num2
-    return operation
+    
+    if operation == "add":
+        result = num1 + num2
+    elif operation == "subtract":
+        result = num1 - num2
+    elif operation == "multiply":
+        result = num1 * num2
+    else:
+        if num2 == 0:
+            result = "Cannot divide by 0"
+        else:
+            result = num1 / num2
+    return result
