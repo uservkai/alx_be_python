a62aa28c5cd32de397f5906a2f6d7591dd654b92
uservkai <katemary6623@gmail.com>
2025-07-26 23:10:45 +0300

    Updated input messages

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -13,11 +13,11 @@ def main():
         
         if choice == '1':
             #Prompt for and add an item
-            item = input ("Add item: ")
+            item = input ("Enter item to add: ")
             shopping_list.append(item)
         elif choice == '2':
             #Prompt for and remove an item
-            item = input ("Remove Item: ")
+            item = input ("Enter item to remove: ")
             if item in shopping_list:
                 shopping_list.remove (item)
             else:
