fc59d7f13e8b7c001872636c9c2758496de6cebb
uservkai <katemary6623@gmail.com>
2025-07-27 20:32:19 +0300

    updated celsius spelling and edited spacing

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,8 +1,8 @@
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
-CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9 /5
 
 def convert_to_celcius(fahrenheit):
-    temp_c = (fahrenheit - 32) * FAHRENHEIT_TO_CELCIUS_FACTOR
+    temp_c = (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
     print(f"{fahrenheit}°F is {temp_c}°C")
     return temp_c
 
