5fabac82b9090610b0d2b191ae2b14f250950998
uservkai <katemary6623@gmail.com>
2025-08-03 01:23:09 +0300

    Fix: adjusted spacing of valueerror return message

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -9,4 +9,4 @@ def safe_divide(numerator, denominator):
         return f"The result of the division is {result:.1f}"
     
     except ValueError:
-        return "Error :Please enter numeric values only."
+        return "Error: Please enter numeric values only."
